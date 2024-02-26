9906d454560d9860a058138eda470002bb28174f
atmacassuleyman <atmacassuleyman@gmail.com>
2024-02-26 01:25:08 +0100

    made some changes.

--- test/constants/physician.constant.ts
+++ test/constants/physician.constant.ts
@@ -14,4 +14,3 @@ export {
 };
 
 export type { Physician };
-
--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -1,17 +1,36 @@
+/**
+ * Waits for an element to be displayed within a specified timeout.
+ * @param elem The WebdriverIO element to wait for.
+ * @param timeOut The maximum time to wait in milliseconds (default is 5000ms).
+ */
 async function waitForElement(elem: WebdriverIO.Element, timeOut = 5000) {
     await elem.waitForDisplayed({timeout: timeOut});
 }
 
+/**
+ * Waits for an element to disappear or not be displayed within a specified timeout.
+ * @param elem The WebdriverIO element to wait for its disappearance.
+ * @param timeOut The maximum time to wait in milliseconds (default is 5000ms).
+ */
 async function waitToDisappear(elem: WebdriverIO.Element, timeOut = 5000) {
     await waitForElement(elem);
     await elem.waitForDisplayed({timeout: timeOut, reverse: true});
 }
 
+/**
+ * Performs a left click on an element after waiting for it to be displayed.
+ * @param elem The WebdriverIO element to click on.
+ * @param timeOut The maximum time to wait for the element to be displayed (default is 5000ms).
+ */
 async function leftClick(elem: WebdriverIO.Element, timeOut = 5000) {
     await waitForElement(elem, timeOut);
     await elem.click();
 }
 
+/**
+ * Returns the current day of the week as a string.
+ * No parameters are needed for this function.
+ */
 function getCurrentDay(): string {
     const date = new Date();
     const dayIndex = date.getDay();
